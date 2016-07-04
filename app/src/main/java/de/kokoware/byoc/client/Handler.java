992d51d97c116f69a21a8eb641e194e6932ade6c
koko1ooo <koko.koko@medionmail.com>
2016-07-04 03:31:55 +0200

    Initial commit

---
+++ app/src/main/java/de/kokoware/byoc/client/Handler.java
@@ -0,0 +1,184 @@
+package de.kokoware.byoc.client;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Created by kokok_000 on 27.06.2016.
+ */
+public class Handler implements Runnable {
+
+    private  Socket cs;
+    private  PrintWriter oupt;
+    private  int position;
+    private int i;
+    private  int bytesread;
+    private  byte[] Buffer;
+    private  String mCurrentPhotoPath;
+    private  File file;
+    private String[] dateinamen;
+    private int A;
+    private Scanner SC_CS;
+    private Boolean Full = true;
+    private int MAX_READ = 0;
+
+
+    public Handler(String[] dateinamen, Socket cs, File file, PrintWriter oupt, int position,int i, int bytesread, byte[] Buffer, String mCurrentPhotoPath ) {
+
+        this.dateinamen = dateinamen;
+        this.cs = cs;
+        this.file = file;
+        this.oupt = oupt;
+        this.position = position;
+        this.i = i;
+        this.bytesread = bytesread;
+        this.Buffer = Buffer;
+        this.mCurrentPhotoPath = mCurrentPhotoPath;
+
+
+
+
+
+    }
+
+
+    @Override
+    public void run() {
+
+
+        try {
+            SC_CS = new Scanner(cs.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // Toast.makeText(MainActivity, "You have selected : " + dateinamen[position] +" to download. Please wait.",
+       //         Toast.LENGTH_LONG).show();
+
+
+        //init
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(cs.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileOutputStream fileout = null;
+
+
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        file = new File(path, dateinamen[position]);
+
+
+
+        oupt.println(2);
+        oupt.println(position+1);
+        A = SC_CS.nextInt();
+
+
+
+
+
+        InputStream nin = null;
+        try {
+            nin = cs.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        i = 0;
+
+        try {
+            fileout = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+
+        try {
+            while (((bytesread = nin.read(Buffer)) > 0) && Full)
+            {
+                fileout.write(Buffer, 0, bytesread);
+
+
+                MAX_READ += bytesread;
+
+
+
+
+                if (MAX_READ >= A)
+                {
+                    Full = false;
+                }
+
+            }
+
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        Log.println(5,"koko", "Nicht weiter als Übertragung");
+        Full = true;
+        MainActivity.Handleract = false;
+
+
+
+        try {
+            fileout.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ app/src/main/java/de/kokoware/byoc/client/MainActivity.java
@@ -0,0 +1,166 @@
+package de.kokoware.byoc.client;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MainActivity extends ListActivity {
+
+    String[] dateinamen;
+
+    public String mCurrentPhotoPath;
+    int Ad, i = 0, A;
+    int Eingegeben;
+    int bytesread;
+    public Socket cs = null;
+    public byte[] Buffer = new byte[16384];
+    public Scanner in = null;
+    public File file;
+    public PrintWriter oupt;
+    public static boolean Handleract;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
+
+        Scanner board = new Scanner(System.in);
+
+
+        Handleract = false;
+
+        try {
+            cs = new Socket("192.168.178.199", 3414);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+        try {
+            in = new Scanner(cs.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        try {
+            oupt = new PrintWriter(cs.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+        oupt.println(1);
+        Ad = in.nextInt();
+
+        dateinamen = new String[Ad];
+
+        while (i < Ad) {
+            dateinamen[i] = in.next();
+            i++;
+        }
+
+        i = 0;
+        while (i < Ad) {
+
+            System.out.print(i + 1);
+            System.out.println(dateinamen[i]);
+            i++;
+        }
+
+
+            System.out.println(dateinamen[1]);
+
+
+
+
+        //Listview adapter
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,dateinamen));
+    }
+    public void onListItemClick(ListView parent, View v,
+                                int position, long id) {
+
+
+
+        if(Handleract == false) {
+            Thread Networking = new Thread(new Handler(dateinamen, cs, file, oupt, position, i, bytesread, Buffer, mCurrentPhotoPath  ));
+            Networking.start();
+            Handleract = true;
+        }
+
+
+
+
+
+        Toast.makeText(this, "File downloaded.",
+                Toast.LENGTH_LONG).show();
+
+
+
+
+    }
+   // android Options Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+
+    public void onGroupItemClick (MenuItem item) {
+        if (item.isChecked()) {
+            item.setChecked(false);
+        } else {
+            item.setChecked(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+}
